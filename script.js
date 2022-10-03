f80c7bb8510d364b70d344ab91c45ce271e0ab78
JacobM23 <84456296+223jmuirhead@users.noreply.github.com>
2022-10-03 13:02:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,147 @@
+//let email = "mario@thenetninja.co.uk";
+
+//let result = email.replace("m", "w");
+
+//let result = email.replace("n", "w");
+
+//console.log(result);
+
+//let radius = 10;
+//const pi = 3.14
+
+// console.log(radius, pi); 
+
+// console.log(10/2);
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+ // let likes = 10;
+
+// likes++;
+
+// likes += 10;
+
+// console.log(likes);
+
+// let result = "the blog has " + likes + " likes.";
+
+// console.log(result);
+
+
+// const title = "Best reads of 2019"
+
+// const author = "Mario";
+
+// const likes = 30;
+
+// let result = "The blog called " + title + " by " + author + " has " + likes + " likes ";
+
+// console.log(result);
+
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+
+// console.log(result);
+
+// let html = `
+  // <h2>${title}</h2>
+  // <p>${author}</p>
+  // <span> This blog has ${likes} likes</span>
+// `;
+
+// let ninjas = ["shaun", "ryu", "chun-li"];
+
+// ninjas[1] = "ken";
+
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+
+// console.log(ages[2]);
+
+// console.log(ninjas.length);
+
+// let result = ninjas.join(', ');
+
+// let result = ninjas.indexOf("chun-li");
+
+// let result = ninjas.concat(["ken", "cyrstal"]);
+
+// let result = ninjas.push("ken");
+// result = ninjas.pop();
+
+// console.log(result);
+
+// let age = null; 
+
+// console.log(age, age + 3, `the age is ${age}`);
+
+// console.log(true, false, "true", "false");
+
+// let email = "luigi@thenetninja.co.uk";
+// let names = ["mario", "luigi", "toad"];
+// // let result = names.includes("luigi");
+
+// // console.log(result);
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age <= 25);
+// console.log(age >= 20);
+
+// let name = "shaun";
+
+// console.log(name == "shaun");
+// console.log(name == "Shaun");
+// console.log(name > "Shaun");
+// console.log(name > "Cyrstal");
+
+// let age = 25;
+
+// console.log(age == 25);
+// console.log(age == "25");
+// console.log(age != 25);
+// console.log(age != "25");
+
+// console.log(age === 25);
+// console.log(age === "25");
+// console.log(age !== 25);
+// console.log(age !== "25");
+
+// let score = "100";
+
+// score = Number(score);
+
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = String(50);
+
+// console.log(result);
+
+// let result = Boolean(100);
+
+// let result = Boolean("");
+
+// console.log(result, typeof result);
+
+//LOOPS
+
+// for(let i = 0; i < 5; i++){
+//   console.log("in loop:", i);
+// }
+
+// console.log("loop finished");
+
+// const names = ["shaun", "mario", "luigi"];
+
+// for(let i = 0; i < names.length; i++){
+//   // console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`
+//   console.log(html);
+// }
+
